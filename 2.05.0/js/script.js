a54f0e434235cbe874f194998786a0b3b6d72292
Betterteng <betterteng@gmail.com>
2018-05-21 18:51:53 +1000

    Delete
    Delete all the useless file…

--- 2.05.0/js/script.js
+++ 2.05.0/js/script.js
@@ -6,25 +6,25 @@
 var width = 1000,
     height = 800;
 
-// Define the div for the tooltip
+// Define the div for the tooltip...
 var div = d3.select("body").append("div")
     .attr("class", "tooltip")
     .style("opacity", 0);
 
+//  Define the size of the canvas...
 var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height);
 
+//  Using force to make sure that all the nodes will be fixed inside the canvas...
 var force = d3.layout.force()
-    .gravity(.05)
     .distance(400)
-    .charge(-100)
     .size([width, height]);
 
-d3.json("data/data.json", function(error, json) {
+d3.json("data/data.json", function(error, json) {  // Read data... Parameter [json] is the return object...
     var edges = [];
 
-    // Transforming...
+    // Transforming (set the source nodes and the target nodes in terms of the data sets and reform into a new array)...
     json.links.forEach(function(e) {
         var sourceNode = json.nodes.filter(function(n) { return n.id === e.node01; })[0],
             targetNode = json.nodes.filter(function(n) { return n.id === e.node02; })[0];
@@ -32,11 +32,12 @@ d3.json("data/data.json", function(error, json) {
         edges.push({source: sourceNode, target: targetNode, value: e.amount});
     });
 
-    console.log(edges);
+    //console.log(edges);
 
-    var min_domain = d3.min(json.links, function(d) {return d.amount;});
-    var max_domain = d3.max(json.links, function(d) {return d.amount;});
+    var min_domain = d3.min(json.links, function(d) {return d.amount;});  //  Find the min value in the array to determine the lower limit for scale methods...
+    var max_domain = d3.max(json.links, function(d) {return d.amount;});  //  Find the max value in the array to determine the upper limit for scale methods...
 
+    // Start the force...
     force
         .nodes(json.nodes)
         .links(edges)
@@ -52,6 +53,7 @@ d3.json("data/data.json", function(error, json) {
         .domain([min_domain, max_domain])
         .range([10, 40]);
 
+    // Set the properties of the links...
     var link = svg.selectAll(".link")
         .data(edges)
         .enter().append("line")
@@ -60,11 +62,13 @@ d3.json("data/data.json", function(error, json) {
             return strokeScaleForLinks(d.value)
         });
 
+    //  Draw the nodes now...
     var node = svg.selectAll(".node")
         .data(json.nodes)
         .enter().append("g")
         .attr("class", "node");
 
+    // Set the attributes of the nodes and the interactions with the mouse...
     node.append("circle")
         .data(edges)
         .attr("class", "node")
@@ -92,12 +96,14 @@ d3.json("data/data.json", function(error, json) {
                 .style("opacity", 0);
         });
 
+    //  Set the title for every node in order to make a clearer visualisation...
     node.append("svg:a")
         .append("text")
         .attr("dx", 12)
         .attr("dy", ".35em")
         .text(function(d) { return d.id});
 
+    //  Draw the links and transform nodes' position...
     force.on("tick", function() {
         link.attr("x1", function(d) { return d.source.x; })
             .attr("y1", function(d) { return d.source.y; })
